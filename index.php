4d58468fa047ef8a4e0e15b65e7a2c290611f685
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-05-03 20:31:39 +0200

    Remade the exercise

--- index.php
+++ index.php
@@ -10,8 +10,15 @@
     <form action="./server.php">
         <p>
             <!-- il nome del for deve coincidere con name e id all interno del input  -->
-            <label for="message">mesaggio</label>
-            <input type="text" name="message" id="message" placeholder="mandaci un messaggio " >
+            <!-- <label for="message">mesaggio</label>
+            <input type="text" name="message" id="message" placeholder="mandaci un messaggio " > -->
+            <div>
+                <div>
+                    <label for="paragrafo">paragrafo</label>
+
+                </div>
+                <textarea name="paragrafo" id="paragrafo" cols="30" rows="10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati molestias, nihil laudantium architecto alias minima quam aliquid quis, incidunt, recusandae ratione pariatur vitae quibusdam neque quae rerum beatae qui dolorum sed quidem itaque unde deleniti nemo! Deserunt repellat tenetur, quibusdam possimus porro modi earum perferendis dolores quisquam quam, pariatur vero!</textarea>
+            </div>
         </p>
        
         <button>invia</button>
--- server.php
+++ server.php
@@ -1,13 +1,35 @@
 <?php
 //per recuperare il valore dai form devi scergliere un metodo (GET/POST) e poi utiliuzarli con il metodo 
-$message = $_GET['message'];
+$message = $_GET['paragrafo'];
 
 $message_lenght = strlen($message);
-$new_message = str_replace($message, '***', $message);
+$new_message = str_replace('appennini', '***', $message);
 $new_message_lenght = strlen($new_message);
 ?>
 
-<h1>Il Messaggio lasciato è: <?php echo $message; ?></h1>
-<h2>La lunghezza del messaggio è: <?php echo $message_lenght; ?></h2>
-<h3>Il nuovo messaggio è: <?php echo $new_message?></h3>
-<h4>La lunghezza del nuovo messaggio è: <?php echo $new_message_lenght ?></h4>
+
+
+<div>
+    <p>
+        il messaggio è:
+
+    </p>
+    <?php echo $message;?>
+</div>
+<p>
+   la lunghezza del messaggio è:  <?php echo $message_lenght; ?> caratteri
+</p>
+<div>
+    <p>
+        Il novo messaggio è:
+        
+    </p>
+    <?php echo $new_message?>
+</div>
+<div>
+    <p>
+        La lunghezza del nuovo paragrafo è:
+        <?php echo $new_message_lenght; ?>
+        caratteri
+    </p>
+</div>
